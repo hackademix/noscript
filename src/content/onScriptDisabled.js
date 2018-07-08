df24338a0446aaf936bd5ad626472177c3aa1cdd
hackademix <giorgio@maone.net>
2018-07-08 14:07:08 +0200

    Use a custom <no-script> element as <NOSCRIPT> replacement.

--- src/content/onScriptDisabled.js
+++ src/content/onScriptDisabled.js
@@ -1,7 +1,7 @@
 function onScriptDisabled() {
   for (let noscript of document.querySelectorAll("noscript")) {
     // force show NOSCRIPT elements content
-    let replacement = document.createElement("span");
+    let replacement = document.createElement("no-script");
     replacement.innerHTML = noscript.innerHTML;
     noscript.parentNode.replaceChild(replacement, noscript);
     // emulate meta-refresh
