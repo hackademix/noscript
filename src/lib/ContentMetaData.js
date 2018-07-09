7dfa2e640bcd5934831920139338b6bb656bcdc5
hackademix <giorgio@maone.net>
2018-07-09 19:23:03 +0200

    Added two library files (one, lib/restricted.js, missing from rc8 tag) and version bump.

---
+++ src/lib/ContentMetaData.js
@@ -0,0 +1,30 @@
+class ContentMetaData {
+  constructor(request, defaultCharset = "utf-8") {
+    this.defaultCharset = defaultCharset;
+    let {responseHeaders} = request;
+    for (let h of responseHeaders) {
+      if (/^\s*Content-(Type|Disposition)\s*$/i.test(h.name)) {
+        this[h.name.split("-")[1].trim().toLowerCase()] = h.value;
+      }
+    }
+  }
+
+  get charset() {
+    let charset = this.defaultCharset;
+    if (this.type) {
+      let m = this.type.match(/;\s*charset\s*=\s*(\S+)/);
+      if (m) {
+        charset = m[1];
+      }
+    }
+    Object.defineProperty(this, "charset", { value: charset, writable: false, configurable: true })
+  }
+
+  createDecoder() {
+    try {
+      return new TextDecoder(charset);
+    } catch (e) {
+      return new TextDecoder(this.defaultCharset);
+    }
+  }
+};
---
+++ src/lib/restricted.js
@@ -0,0 +1,29 @@
+{
+  // see https://bugzilla.mozilla.org/show_bug.cgi?id=1415644
+  let domains = [
+  "accounts-static.cdn.mozilla.net",
+  "accounts.firefox.com",
+  "addons.cdn.mozilla.net",
+  "addons.mozilla.org",
+  "api.accounts.firefox.com",
+  "content.cdn.mozilla.net",
+  "content.cdn.mozilla.net",
+  "discovery.addons.mozilla.org",
+  "input.mozilla.org",
+  "install.mozilla.org",
+  "oauth.accounts.firefox.com",
+  "profile.accounts.firefox.com",
+  "support.mozilla.org",
+  "sync.services.mozilla.com",
+  "testpilot.firefox.com",
+  ];
+
+  function isRestrictedURL(u) {
+    try {
+      if (typeof u === "string") u = new URL(u);
+      return u.protocol === "https:" && domains.includes(u.hostname);
+    } catch (e) {
+      return false;
+    }
+  }
+}
