99a81a6f1f1cbc6b2dd6ccaf8bbe40dbe35f83b1
hackademix <giorgio@maone.net>
2018-09-05 11:38:59 +0200

    Stop tracking tld.js build-time intermediate files.

--- TLD/try.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-perl -ne 'if (! /^(\/\/|!|[ \n\r])/) { s/\n/\|/; s/\./\\\./g ; s/\*\\\./[^\\.]+\\./; s/\s+utf.*/|/; print }' *.dat > tld_rx.txt
-perl -ne 'if (/^!/) { s/\n/\|/;  s/\./\\\./g ; s/^!//; s/\s+utf.*/|/; print }' *.dat > tld_ex.txt
