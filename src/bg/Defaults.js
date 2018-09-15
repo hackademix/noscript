e4e1c83f2cac33341d9ad132dea8ca90c15487a4
hackademix <giorgio@maone.net>
2018-09-15 22:10:19 +0200

    Fixed line endings.

---
+++ src/bg/Defaults.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Defaults = {
+  async init()  {
+    let defaults = {
+      local: {
+        debug: false,
+        showCtxMenuItem: true,
+        showCountBadge: true,
+        showFullAddresses: false,
+      },
+      sync: {
+        "global": false,
+        "xss": true,
+        "clearclick": true
+      }
+    };
+    let defaultsClone = JSON.parse(JSON.stringify(defaults));
+
+    for (let [k, v] of Object.entries(defaults)) {
+      let store = await Storage.get(k, k);
+      if (k in store) {
+        Object.assign(v, store[k]);
+      }
+      v.storage = k;
+    }
+
+    Object.assign(ns, defaults);
+
+    // dynamic settings
+    if (!ns.local.uuid) {
+      ns.local.uuid = uuid();
+      await ns.save(ns.local);
+    }
+
+    return ns.defaults = defaultsClone;
+  }
+};
