2fa009673f3d51b14389f8e25075de394290b32c
hackademix <giorgio@maone.net>
2019-02-01 01:13:26 +0100

    Conditional CSS toggle for non-mozilla browsers.

--- src/lib/UA.js
+++ src/lib/UA.js
@@ -2,7 +2,11 @@ var UA = {
   isMozilla: document.URL.startsWith("moz-"),
 }
 
-if (!UA.isMozilla && typeof chrome === "object" && !chrome.tabs && typeof exportFunction === "undefined") {
-  // content script shims
-  window.exportFunction = () => {};
+if (!UA.isMozilla) {
+  if (typeof chrome === "object" && !chrome.tabs && typeof exportFunction === "undefined") {
+    // content script shims
+    window.exportFunction = () => {};
+  }
+} else {
+  document.documentElement.classList.add("mozwebext");
 }
