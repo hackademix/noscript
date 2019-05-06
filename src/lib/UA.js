c2be460915714a1696a9c5c4f98e40f2dbb3be8e
hackademix <giorgio@maone.net>
2019-05-06 12:11:31 +0200

    Fix mozwebext class added to content pages (Issue #80).

--- src/lib/UA.js
+++ src/lib/UA.js
@@ -1,12 +1,22 @@
-var UA = {
-  isMozilla: document.URL.startsWith("moz-") || typeof window.wrappedJSObject === "object",
-}
-
-if (!UA.isMozilla) {
-  if (typeof chrome === "object" && !chrome.tabs && typeof exportFunction === "undefined") {
-    // content script shims
-    window.exportFunction = () => {};
+{
+  let mozWebExtUrl = document.URL.startsWith("moz-");
+  let isMozilla = mozWebExtUrl || window.wrappedJSObject === "object";
+  if (isMozilla) {
+    if (mozWebExtUrl) {
+      // help browser-specific UI styling
+      document.documentElement.classList.add("mozwebext");
+    }
+  } else {
+    // shims for non-Mozilla browsers
+    if (typeof chrome === "object" && !chrome.tabs) {
+      // content script shims
+      if (typeof exportFunction === "undefined") {
+        window.exportFunction = () => {};
+      }
+    }
   }
-} else {
-  document.documentElement.classList.add("mozwebext");
+
+  var UA = {
+    isMozilla
+  };
 }
