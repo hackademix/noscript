7aeac83eeb2ad7e5e499eacf0358f23a65373918
hackademix <giorgio@maone.net>
2022-02-08 11:01:03 +0100

    [XSS] Simplified preemptive name sanitization.

--- src/xss/sanitizeName.js
+++ src/xss/sanitizeName.js
@@ -20,10 +20,10 @@
 
 ns.on("capabilities", event => {
   if (ns.allows("script")) {
-    let name = ns.getWindowName();
-    if (/[<"'\`(=:]/.test(name)) {
-      console.log(`NoScript XSS filter sanitizing suspicious window.name "%s" on %s`, name, document.URL);
-      window.name = window.name.substring(0, window.name.length - name.length);
+    let dangerousRx = /[<"'\`(=:]/g;
+    if (/[<"'\`(=:]/.test(window.name)) {
+      console.log(`NoScript XSS filter sanitizing suspicious window.name "%s" on %s`, window.name, document.URL);
+      window.name = window.name.replace(dangerousRx, '');
     }
   }
 });
