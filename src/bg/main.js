cd30505fe148f6db7e4ed1e5c7f98c72c038e612
hackademix <giorgio@maone.net>
2022-02-14 18:50:58 +0100

    Fallback context to same URL when documentUrl is missing.

--- src/bg/main.js
+++ src/bg/main.js
@@ -231,9 +231,9 @@
     policyContext(contextualData) {
       // contextualData (e.g. a request details object) must contain a tab, a tabId or a documentUrl
       // (used as a fallback if tab's top URL cannot be retrieved, e.g. in service workers)
-      let {tab, tabId, documentUrl} = contextualData;
+      let {tab, tabId, documentUrl, url} = contextualData;
       if (!tab) tab = tabId !== -1 && TabCache.get(tabId);
-      return tab && tab.url || documentUrl;
+      return tab && tab.url || documentUrl || url;
     },
     requestCan(request, capability) {
       return !this.isEnforced(request.tabId) || this.policy.can(request.url, capability, this.policyContext(request));
